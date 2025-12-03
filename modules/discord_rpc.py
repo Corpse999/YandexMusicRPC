693ed4fedd60936a95ba4107d030d8708439d199
Corpse <andreyeliseev21o@gmail.com>
2025-12-04 02:06:24 +0300

    Было принято решение перейти с discordrpc на форк pypresence в связи с его удобством

--- modules/discord_rpc.py
+++ modules/discord_rpc.py
@@ -1,20 +1,19 @@
 import time
 import random
-import discordrpc
+from pypresence import Presence
 from modules.config import LoadConfig
-from discordrpc import Activity, StatusDisplay
+from pypresence import StatusDisplayType
+from pypresence.types import ActivityType
 from modules.ym_functions import YandexMusicInfo
-
 
 class DiscordRPC:
     def __init__(self):
         self.ym_info_class = YandexMusicInfo()
         self.client_id = LoadConfig().discord_rpc_client_id
-        self.rpc = discordrpc.RPC(self.client_id, output=False)
-        self.default_ym_url = "https://music.yandex.ru/track/"
+        self.rpc = Presence(client_id=self.client_id)
         self.time_started = None
-        self.prev_track = None
-        self.change_rpc = 0
+        self.prev_track_title = None
+        self.change_rpc = False
 
         self.random_statuses = [
             "В поисках вдохновения...",
@@ -22,12 +21,15 @@ class DiscordRPC:
         ]
 
         # По умолчанию ---------------------------
+        self.default_ym_url = "https://music.yandex.ru/track/"
         self.default_thumbnail = "https://github.com/Corpse999/YandexMusicRPC/blob/test/resources/ym_wave.gif?raw=true"
 
         self.default_assets = {
             "small_text": "GitHub: Corpse999",
             "small_image": "https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png",
-            "small_url": "https://github.com/Corpse999/YandexMusicRPC"
+            "small_url": "https://github.com/Corpse999/YandexMusicRPC",
+            "activity_type": ActivityType.LISTENING,
+            "status_display_type": StatusDisplayType.DETAILS, 
         }
 
         self.default_rpc = {
@@ -37,13 +39,16 @@ class DiscordRPC:
         # ----------------------------------------
 
     def parse_time(self, time):
-        time_list = time.split(":")
-        if len(time_list) > 3:
-            raise Exception("Слишком большая строка времени. Максимум - часы:минуты:секунды")
-        seconds = int(time_list[-1]) if len(time_list) >= 1 else 1
-        minutes = int(time_list[-2]) if len(time_list) >= 2 else 0
-        hours = int(time_list[-3]) if len(time_list) == 3 else 0
-        return seconds + minutes*60 + hours * 3600
+        try:
+            time_list = time.split(":")
+            if len(time_list) > 3:
+                raise Exception("Слишком большая строка времени. Максимум - часы:минуты:секунды")
+            seconds = int(time_list[-1]) if len(time_list) >= 1 else 1
+            minutes = int(time_list[-2]) if len(time_list) >= 2 else 0
+            hours = int(time_list[-3]) if len(time_list) == 3 else 0
+            return seconds + minutes*60 + hours * 3600
+        except:
+            return None
     
     def parse_track_url(self, track_url):
         try:
@@ -64,8 +69,8 @@ class DiscordRPC:
             "details_url": self.parse_track_url(self.ym_info_class.current_track.get("track_url")),
             "state": ", ".join(self.ym_info_class.current_track.get("authors")),
             "large_image": thumbnail,
-            "ts_start": self.time_started,
-            "ts_end": tot_time
+            "start": self.time_started,
+            "end": tot_time
         }  
 
         custom_rpc = {**custom_rpc, **self.default_assets}
@@ -74,54 +79,49 @@ class DiscordRPC:
         if self.ym_info_class.current_track.get("status") == "paused":
             custom_rpc["small_image"] = "https://github.com/Corpse999/YandexMusicRPC/blob/main/static/paused.png?raw=true"
             custom_rpc["small_text"] = "На паузе"
-            custom_rpc["small_url"], custom_rpc["ts_start"], custom_rpc["ts_end"] = None, None, None
+            custom_rpc["small_url"], custom_rpc["start"], custom_rpc["end"] = None, None, None
 
         return custom_rpc
 
 
     def run_rpc(self):
+        self.rpc.connect()
         while True:
-            # Делаем двойную проверку: если отсутствуют название и авторы, то ставим дефолтный RPC
-            if ((track_title:=self.ym_info_class.current_track.get("title")) is None and not self.ym_info_class.current_track.get("authors")):
-                self.rpc.set_activity(**self.default_rpc,
-                                      **self.default_assets,
-                                      status_type=StatusDisplay.Details,
-                                      act_type=Activity.Listening)
-            elif not self.ym_info_class.current_track.get("time").get("current"):
+            track = self.ym_info_class.current_track
+            title = track.get("title")
+            authors = track.get("authors")
+            current_time = self.parse_time(track.get("time", {}).get("current"))
+
+            # Ставим дефолтный RPC, если не удается получить авторов и название
+            if (title is None and not authors) or current_time is None:
+                self.rpc.update(**self.default_rpc,
+                                **self.default_assets)
+                self.prev_track_title = None
+                self.time_started = None
                 continue
-            else:
-                parsed_time = self.parse_time(self.ym_info_class.current_track.get("time").get("current", 0))
-                # Время из ЯМ (не из RPC)
-                alternative_time_started = round(time.time() - parsed_time)
-                # Задаем время начала трека
-                if self.time_started is None: 
-                    self.time_started = alternative_time_started
-                
-                # Текущее время, которое показывает RPC
-                cur_time_discord = time.time() - (self.time_started or 0)
 
-                # Если абсолютная разница текущего времени из ЯМ и текущего времени из RPC отличается на > 2, то вставляем время из ЯМ
-                if abs(cur_time_discord - parsed_time) > 2:
-                    self.time_started = alternative_time_started
-                    self.change_rpc = 1
+            # Если трек поменялся, то скидываем время и ставим метку, что надо поменять RPC
+            if title != self.prev_track_title:
+                self.prev_track_title = title
+                self.change_rpc = True
+                self.time_started = int(time.time() - current_time)
 
-                # Делаем проверку, равен ли текущий тайтл прошлому. Если нет - меняем время и предыдущий тайтл, а так же обновляем RPC
-                if self.prev_track != track_title: 
-                    self.prev_track = track_title
-                    self.time_started = alternative_time_started
-                    self.change_rpc = 1
+            # Сверяем текущее время в RPC и реальное время RPC. Если разница > 2 сек, то меняем время
+            discord_time = time.time() - self.time_started
+            if abs(discord_time - current_time) > 2:
+                self.time_started = int(time.time() - current_time)
+                self.change_rpc = True
 
-                if self.change_rpc == 1:
-                    self.rpc.set_activity(**self.custom_rpc,
-                                          status_type=StatusDisplay.Details,
-                                          act_type=Activity.Listening,)
-                    self.change_rpc = 0
-
+            if self.change_rpc:
+                self.rpc.update(**self.custom_rpc)
+                self.change_rpc = False
+            
+            # Спим перед новым циклом
             time.sleep(0.2)
+
 
     def run_loop(self, ws_url):
         print("Discord RPC был успешно запущен!")
-        print(f"Подключено к: {self.rpc.User.name} ({self.rpc.User.id})")
         self.ym_info_class.start_ws_client(ws_url)
         self.run_rpc()
     
